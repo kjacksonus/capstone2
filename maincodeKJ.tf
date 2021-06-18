0355874bf4ca1bed3e199458f80830318f012585
EC2 Default User <ec2-user@ip-172-31-28-86.ec2.internal>
2021-06-18 21:38:13 +0000

    see if works

---
+++ maincodeKJ.tf
@@ -0,0 +1,19 @@
+# #provision vpc, all subnets, igw, and default (main) route-table 
+# #VPC-1, 6 subnets (includes duplication of infrastructure; web -pub-,app, and dbase)
+# #provision corporate vpc 
+resource "aws_vpc" "go_green_corp_vpc" {
+  cidr_block           ="10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "GoGreenCorpVPC"
+  }
+}
+
+# Create IGW (Internet Gateway)
+resource "aws_internet_gateway" "go_green_igw" {
+    vpc_id = aws_vpc.go_green_corp_vpc.id
+    tags = {
+      Name = "GoGreenCorpIGW"
+    }
+
+}
